501ed9f901b2f39e42f06361e53477635a205804
Dmtome <dmtome@naver.com>
2023-02-04 20:15:53 +0900

    add hello world

--- Hello/src/test1/test.java
+++ Hello/src/test1/test.java
@@ -3,8 +3,9 @@ package test1;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Test!");
+        System.out.println("Test1");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("hello world!");
     }
 }
