fa21cd9eee9cc4dfe81c5341e0e05bce0e06be21
Dmtome <dmtome@naver.com>
2023-02-15 23:30:59 +0900

    p2

--- Hello/src/practice1/Test.java
+++ Hello/src/practice1/Test.java
@@ -3,7 +3,8 @@ package practice1;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println();
+		System.out.println("practice2");
 
 	}
 
